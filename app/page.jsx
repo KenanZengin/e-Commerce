0f44f126c2840094755ffbbb98d4e8a6f7f62bc6
KenanZengin <112508321+KenanZengin@users.noreply.github.com>
2023-09-23 19:57:46 +0300

    css set up ready

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
 
-import Logo from 'public/img/mainLogo.png'
 const HomePage = () => {
   return (
     <div> 
-      <Image src={Logo} width={170} />
+     
     </div>
   )
 }
