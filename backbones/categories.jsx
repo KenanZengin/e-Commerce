45e6d7fa89afb627ba731c737bdf9571fc899302
KenanZengin <112508321+KenanZengin@users.noreply.github.com>
2023-10-14 23:39:06 +0300

    categories update

--- backbones/categories.jsx
+++ backbones/categories.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -24,41 +25,47 @@ import categori_18 from 'public/img/categories/categori_18.png'
 import categori_19 from 'public/img/categories/categori_19.png'
 import categori_20 from 'public/img/categories/categori_20.png'
 import categori_21 from 'public/img/categories/categori_21.png'
+import { useEffect, useState } from 'react'
 
 
 const Categories = () => {
 
+    const [deneme,deneme1] = useState(true)
+    useEffect(()=>(
+        deneme1(true)
+    ),[deneme])
     const imageList = [{img : categori_1 , categoriName : "Arts & Crafts" ,link:"/"},{img : categori_2 , categoriName : "Baby" ,link:"/"},{img : categori_3 , categoriName : "Books" ,link:"/"},{img : categori_4 , categoriName : "Car & Other Vehicles" ,link:"/"},{img : categori_5 , categoriName : "Clothing & Accessories" ,link:"/"},{img : categori_6 , categoriName : "Electronics & Tech" ,link:"/"},{img : categori_7 , categoriName : "Food & Beverage" ,link:"/"},{img : categori_8 , categoriName : "Footwear" ,link:"/"},{img : categori_9 , categoriName : "Garden & Patio" ,link:"/"},{img : categori_10 , categoriName : "Gift Cards" ,link:"/"},{img : categori_11, categoriName : "Handmade" ,link:"/"},{img : categori_12 , categoriName : "Health & Beauty" ,link:"/"},{img : categori_13 , categoriName : "Home" ,link:"/"},{img : categori_14 , categoriName : "Industrial Tools & Products" ,link:"/"},{img : categori_15 , categoriName : "Jewelery & Watches" ,link:"/"},{img : categori_16 , categoriName : "Musical Instuments" ,link:"/"},{img : categori_17 , categoriName : "Office & School" ,link:"/"},{img : categori_18 , categoriName : "Pets &  Other Animals" ,link:"/"},{img : categori_19 , categoriName : "Sports & Outdoor Activities" ,link:"/"},{img : categori_20 , categoriName : "Tools & Handware" ,link:"/"},{img : categori_21 , categoriName : "Toys & Play" ,link:"/"}]
 
 
   return (
   <>
-    <div className="categories">
+    {deneme && <div className="categories" >
         <div className="categories-left">
             <p>SHOP NOW</p>
             <h3>Shop<br />everything<br />you need<br />online</h3>
         </div>
         <div className="categories-right">
-        
             <p>
                 <span>ALL DEPARTMANS</span>    
                 <label htmlFor="close-btn"><AiOutlineClose size={32} /></label>
-            </p>     
+            </p>            
             <div className="categories-list">
                 {imageList.map((item)=>(
-                    <Link href={item.link} className="list-item" key={item.categoriName}>
+                    <Link href={item.link} className="list-item" key={item.categoriName} onClick={()=>deneme1(!deneme)}>
                         <Image src={item.img} alt='categori' />
                         <span>{item.categoriName}</span>
                     </Link> 
                 ))}
-                <Link href={"/categories"} className="list-item">
+                
+                <Link href={"/categories"} className="list-item" onClick={()=>deneme1(!deneme)}>
                     <p>See All Categories</p>
                     <HiArrowNarrowRight size={25} />
+                    
                 </Link>        
+                
             </div>
         </div>        
-    </div>
-
+    </div>}
   </>
   )
 }
