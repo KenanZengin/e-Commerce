f68cc81347462504997686797d15283b7d8569c4
KenanZengin <112508321+KenanZengin@users.noreply.github.com>
2023-11-20 23:54:54 +0300

    search on

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en">   
       <Providers>
           <body className={Red_HAT.className}>
             <div className="body-wrapper">
--- backbones/categorisearch.jsx
+++ backbones/categorisearch.jsx
@@ -1,25 +1,45 @@
+"use client"
 import Link from "next/link"
+import {useState} from "react"
 import {FiSearch} from "react-icons/fi"
 import { FaFire } from "react-icons/fa";
 
 
 const CategoriSearch = () => {
+
     const categories = ["Art","Baby","Books","Car","Electronics","Food","Footwear","Garden","Gift","Home","Pets","Toys","Tools","Musical Instuments"]
+
+    const [searchTerm,setSearchTerm] = useState("")
+    const [searchResult,setSearchResult] = useState(categories)
+
+    const handleSearch = (e) => {
+        const userInput = e.target.value.toLowerCase()
+        setSearchTerm(userInput)
+    
+        const results = categories.filter((item) => item.toLowerCase().includes(userInput))
+
+        //const turkishResults = results.filter((item) => 
+            //item.toLowerCase().normalize('NFC').replace(/[\u0300-\u036f]/g, '').includes(userInput)
+        //)
+        setSearchResult(results)
+    }
+
+
 
   return (
     <>
     
         <label htmlFor="search">
             <FiSearch size={24} />
-            <input type="text" id="search" name="search" placeholder='Search Products' />
+            <input type="text" id="search" name="search" placeholder='Search Products'  value={searchTerm} onChange={handleSearch} />
         </label>
         <div className="search-list">
             <span className="search-list-title">Categories</span>
             <ul>
-               {categories.map((item,i) => (  
+               {searchResult.map((item,i) => (  
                     i < 5 
                         ? 
-                            <li>
+                            <li key={i}>
                                 <Link href={"/"}>
                                     {item}
                                 </Link>
